33c4228e4bc6afeb4cef2b11abeefdac39f6d0ad
AdityaGawali <adityagawali13@gmail.com>
2022-11-06 00:22:10 -0400

    add header

---
+++ data/json_add_hyperlink.py
@@ -0,0 +1,22 @@
+import json
+
+
+f = open("overview.json", encoding="utf8")
+data = json.load(f)
+json_list = []
+json_file = open("json_with_hyperlink.json","w")
+count = 0
+for asn_str in data:
+    asn_str['asn'] = "<button class=\"btn btn-outline-secondary btn-sm\" onClick=\"asn_details(" + str(asn_str['asn']) + ")\">" + str(asn_str['asn']) + "</button>"
+    asn_str['ratio']  = asn_str['ratio']*100
+    #json.dump(asn_str, json_file, indent=4, separators=(',',': '))
+    json_list.append(asn_str)
+    # count = count+1
+    # if(count>10):
+    #     break
+
+
+jsonString = json.dumps(json_list, indent=4)
+json_file.write(jsonString)
+f.close()
+json_file.close()
